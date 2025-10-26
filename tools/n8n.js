32714eee1e49dc6ef151b6237c2485c93d34ee87
abdelbarie141-ou <abdelbarie141@gmail.com>
2025-10-26 13:17:50 +0100

    Update n8n.js

--- tools/n8n.js
+++ tools/n8n.js
@@ -5,7 +5,7 @@ import fetch from "node-fetch";
  * @param {Object} input - The data to send to n8n workflow
  * @returns {Object} - JSON response from n8n
  */
-export async function runN8n(input) {
+export async function handleN8n(input) {
   try {
     const N8N_WEBHOOK = process.env.N8N_WEBHOOK; // URL of your n8n workflow webhook
 
